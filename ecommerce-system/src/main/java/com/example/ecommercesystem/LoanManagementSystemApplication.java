e6b141539303ebaca25d846b2e2fdcb1ce77cbd5
Amit <fulldev101@gmail.com>
2024-07-22 19:42:56 +0530

    Added cache related changes

--- ecommerce-system/src/main/java/com/example/ecommercesystem/LoanManagementSystemApplication.java
+++ ecommerce-system/src/main/java/com/example/ecommercesystem/LoanManagementSystemApplication.java
@@ -3,8 +3,11 @@ package com.example.ecommercesystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+//@SpringBootApplication
 public class LoanManagementSystemApplication {
 
 	public static void main(String[] args) {
--- ecommerce-system/src/main/java/com/example/ecommercesystem/service/ProductService.java
+++ ecommerce-system/src/main/java/com/example/ecommercesystem/service/ProductService.java
@@ -5,6 +5,9 @@ import com.example.ecommercesystem.exception.ResourceNotFoundException;
 import com.example.ecommercesystem.model.Product;
 import com.example.ecommercesystem.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,10 +17,15 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    // Cache the list of all products
+    @Cacheable(value = "products")
     public List<Product> getAllProducts(){
         return productRepository.findAll();
     }
 
+    // Save a product and update the cache
+    @CachePut(value = "product", key = "#product.id")
+    @CacheEvict(value = "products", allEntries = true)
     public Product saveProduct(Product product){
         return productRepository.save(product);
     }
@@ -27,8 +35,15 @@ public class ProductService {
 
     }
 
+    // Cache a product by ID
+    @Cacheable(value = "product", key = "#id")
     public Product getProductById(Long id) {
         return productRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Product not found with id " + id));
+    }
+
+    @CacheEvict(value = "products", allEntries = true)
+    public void evictAllProductsCache() {
+        // This method will clear the 'products' cache
     }
 
 
